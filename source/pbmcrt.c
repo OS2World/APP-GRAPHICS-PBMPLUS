35dd5292a2924deee9bcaa9f4eb1f1deac3d8525
martiniturbide <martiniturbide@gmail.com>
2013-11-21 12:53:46 -0500

    original source code.

---
+++ source/pbmcrt.c
@@ -0,0 +1,13 @@
+/*
+
+This is the C run time dll for pbmplus
+
+*/
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void pbmcrt (void) {
+	fprintf (stderr, "This is the C run time dll for pbmplus.\n");
+}
